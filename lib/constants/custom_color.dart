232fc706856b2757f30515c56bfe77fda07795b5
Nima <148116199+SmallProgrammer89@users.noreply.github.com>
2023-12-07 14:55:10 +0330

    Update custom_color.dart

--- lib/constants/custom_color.dart
+++ lib/constants/custom_color.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 class CustomColor {
   static const Color customRed = Color(0xffE60023);
   static Color customGrey = Colors.grey.shade100;
-  static Color textColor = CustomColor.textColor;
+  static Color textColor = Colors.grey.shade300;
 }
